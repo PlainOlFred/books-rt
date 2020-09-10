d0b0f5b025e15f85541c63d0563817113d866927
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-09-10 18:18:55 -0500

    added mongoose model Book

---
+++ models/book.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = Schema({
+    title:{
+        type: String
+    },
+    subtitle:{
+        type: [String]
+    },
+    authors: {
+        type: String
+    },
+    desc: {
+        type: String
+    },
+    image: {
+        type: String
+    }, googleId: {
+        type: String,
+        unigue: true
+    }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
